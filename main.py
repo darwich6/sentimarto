9699c7d506c47558ee345b51cf9258d063cd49ab
Ahmed Darwich <ad263@uakron.edu>
2023-04-18 21:33:18 -0400

    feat: split dataframe into art_styles

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pandas
 
 def read_csv():
-    df = pandas.read_csv("Contrastive.csv");
-    print(df.to_csv(index=False))
+    df = pandas.read_csv("Contrastive.csv")
+
+    # group the dataframe by art_style and create a dictionary of dataframes
+    art_style_groups = df.groupby('art_style')
+    dfs_dict = {}
+    for art_style, group in art_style_groups:
+        dfs_dict[art_style] = group
+
+    # print the new dataframes
+    # for key, value in dfs_dict.items():
+    #    print(f"Dataframe for {key}:")
+    #    print(value)
+    #    print()
+
+    return dfs_dict
+
+
 
 read_csv()
