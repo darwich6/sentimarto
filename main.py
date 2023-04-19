008aec155318a37ca1bb4184eb62d03c23af18a9
Ahmed Darwich <ad263@uakron.edu>
2023-04-18 23:15:09 -0400

    feat: implement sentiment analysis

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 def read_csv():
     df = pandas.read_csv("Contrastive.csv")
@@ -9,14 +12,34 @@ def read_csv():
     for art_style, group in art_style_groups:
         dfs_dict[art_style] = group
 
+    # print(df['emotion'].unique().tolist())
     # print the new dataframes
     # for key, value in dfs_dict.items():
     #    print(f"Dataframe for {key}:")
     #    print(value)
     #    print()
 
-    return dfs_dict
+    sentiment_analysis(dfs_dict)
 
 
+def sentiment_analysis(dfs_dict):
+    nltk.download('vader_lexicon')  # download VADER lexicon
+
+    # create sentiment analyzer
+    analyzer = SentimentIntensityAnalyzer()
+
+    sentiments = {}
+
+    for art_style, df in dfs_dict.items():
+        art_style_sentiments = []
+        for index, row in df.iterrows():
+            utterance = row['utterance']
+            anchor_painting = row['anchor_painting']
+            sentiment_score = analyzer.polarity_scores(utterance)['compound']
+            art_style_sentiments.append({'art_style': art_style, 'utterance': utterance,
+                                         'anchor_painting': anchor_painting, 'sentiment_score': sentiment_score})
+        sentiments[art_style] = art_style_sentiments
+
+    print(sentiments)
 
 read_csv()
